6359855a397a2054df8b878391beafaee2136ab0
chanryu32 <92428854+chanryu32@users.noreply.github.com>
2023-03-28 14:13:00 +0900

    nameは使用しないので削除

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -10,38 +10,31 @@ import QuizManagementEdit from './components/quiz-management/Edit';
 const routes = [
   {
     path: '/home',
-    name: 'home',
     component: Home,
   },
   {
     path: '/quiz',
-    name: 'quizIndex',
     component: QuizIndex,
   },
   {
     path: '/quiz/show/:id',
-    name: 'quizShow',
     component: QuizShow,
     props: true,
   },
   {
     path: '/quiz-management',
-    name: 'quiz-manegementIndex',
     component: QuizManagementIndex,
   },
   {
     path: '/quiz-management/show/:id',
-    name: 'quizManegementShow',
     component: QuizManagementShow,
     props: true,
   },
   {
     path: '/quiz-management/create',
-    name: 'quizManegementCreate',
     component: QuizManagementCreate,
   }, {
     path: '/quiz-management/edit/:id',
-    name: 'quizManegementEdit',
     component: QuizManagementEdit,
     props: true,
   },
