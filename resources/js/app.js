60fdf322a0444d7336c6f307ee76d063bff508f7
chanryu32 <92428854+chanryu32@users.noreply.github.com>
2023-03-30 18:44:51 +0900

    不要なコードの削除

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,8 @@
 require('./bootstrap');
 
 import { createApp } from 'vue';
-import IndexComponent from './components/quiz/Index';
 import router from './routes';
 
 createApp({
-  components: {
-    'v-index': IndexComponent
-  }
 }).use(router)
   .mount('#app');
